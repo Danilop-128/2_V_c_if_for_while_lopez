ac28cc937c52a08e528296dd3d16d1ecb1558274
Danielalopez-128 <a.22308051281240@cbtis128.edu.mx>
2024-09-06 09:33:11 -0600

    Add files via upload
    subí dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,27 @@
+'''daniela lópez
+Trabajando con cadenas
+'''
+
+nombre= "Daniela"
+print(nombre[1])
+print(nombre)
+for c in "Daniela":
+  print(c)
+
+MisNovias=["Agripina","Anastacia","Maria"]
+for chicas in MisNovias:
+  print(chicas)
+
+  print("otro ejemplo")
+  b ="Daniela López"
+print(b[2:5])
+
+print("a mayusculas")
+a = "Daniela López"
+print(a.upper())
+
+print("Concatenar")
+a = "María"
+b = "Perez"
+c = a +" "+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,17 @@
+import math
+edad=65  # 65 se guarda en edad
+
+nombre="Paola Ramirez"
+print("Tu edad es",edad)
+print("Te llamas",nombre)
+print("-- tipo de variables")
+x = str(7)    # x will be '3'
+y = int(3.1416)    # y will be 3
+z = float(7)
+print(x)
+print(y)
+print(z)
+print("--Funciones matematicas--")
+x = math.sqrt(64)
+
+print(x)
